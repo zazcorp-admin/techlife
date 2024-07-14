1277e95324b2c550d07303b813656a8691d584de
devzaz <74811448+zazcorp-admin@users.noreply.github.com>
2024-07-14 22:12:34 +0600

    theme implemented

--- techlife/urls.py
+++ techlife/urls.py
@@ -23,7 +23,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 
